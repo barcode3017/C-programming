5aed4e52f35950acaa78e1f51d9ef39425d4e7d3
최재혁 <66003250+jaehyeok3017@users.noreply.github.com>
2021-05-22 01:48:04 +0900

    Update Star_Master.c

--- Star_Master.c
+++ Star_Master.c
@@ -8,12 +8,33 @@ int main() {
 
   // 기본 별 찍기 ( a*b 정사각형 모양 )
   printf("\n< 방법 1 > 기본 모양으로 별 찍기\n");
-  for(i = 0; i<b; i++){
+  for(i=0; i<b; i++){
     for(j=0; j<a; j++){
       printf("*");
     }
     printf("\n");
   }
+  
+  
+  // b*a 직사각형 모양으로 별 찍기
+  printf("\n< 방법 2 > 기본 모양의 역방향으로 별 찍기\n");
+  for(i=0; i<a; i++){
+    for(j=0; j<b; j++){
+      printf("*");
+    }
+    printf("\n");
+  }
+  
+  
+  // a의 줄 수만큼 별 찍기
+  printf("\n< 방법 3 > a의 줄 수 만큼 별 찍기\n");
+  for(i=0; i<a; i++){
+      for(j=a; j>=a; j--){
+          printf("*");
+      }
+      printf("\n");
+  }
+  
   
   return 0;
 }
